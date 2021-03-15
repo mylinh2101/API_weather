c1a9da25429f458e8b1a8d2d28dfe28c767e69c7
DoanLeMyLinh <mylinh210100@gmail.com>
2021-03-16 00:54:32 +0700

    push code

---
+++ DoanLeMyLinh_5951071049/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using DoanLeMyLinh_5951071049.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DoanLeMyLinh_5951071049.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET: api/Student
+        public IEnumerable<Student> Get()
+        {
+            var studentList = new List<Student>();
+            //  for (int i = 0; i < 10; i++)
+            //{
+            var student = new Student
+            {
+                Class = $"CQ.59.CNTT",
+                Name = $"Doan Le My Linh",
+                ID = $"5951071049",
+                Address = $"Ho Chi Minh city",
+                DateTime = DateTime.Now.ToUniversalTime()
+            };
+            studentList.Add(student);
+            //  }
+
+            return studentList;
+        }
+
+        // GET: api/Student/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Student
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Student/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Student/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DoanLeMyLinh_5951071049/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace DoanLeMyLinh_5951071049.Models
+{
+    [DataContract]
+    public class Student
+    {
+        [DataMember(Name = "Class")]
+        public string Class { get; set; }
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+        [DataMember(Name = "ID")]
+        public string ID { get; set; }
+        [DataMember(Name = "Address")]
+        public string Address { get; set; }
+        [DataMember(Name = "dateTime")]
+        public DateTime DateTime { get; set; }
+    }
+}
